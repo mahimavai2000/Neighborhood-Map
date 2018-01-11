3386b8b740286ce67f5ca4ddcdbcc5101c25eff4
Uma <mahimavai2000@gmail.com>
2018-01-11 18:27:16 -0500

    Commit to confirm doubts

--- js/app.js
+++ js/app.js
@@ -41,9 +41,9 @@ var locations= [
 	location: {lat:40.782045, lng: -73.971711}
 },
 {
-	title: 'Bronx Zoo',
+	title: 'Grand Central Terminal',
 	img: '',
-	location: {lat:40.850288, lng: -73.878611}
+	location: {lat:40.752570, lng: -73.977627}
 },
 {
 	title: 'Brooklyn Botanic Garden',
@@ -51,32 +51,47 @@ var locations= [
 	location: {lat:40.667622, lng: -73.963191}
 }
 ];
+
 var map,marker,infoWindow;
-var markers=[];
+//Knockout View Model
+//Create Knockout ViewModel to list out the locations
+function ViewModel() {
+  var self=this;
+  
+  var markers=[];
+
+  //create KO observable array to hold locations array data
+  this.locationList= ko.observableArray([]);
+
+  //Add locations details into KO observable array
+  locations.forEach(function(locationDetails){
+    self.locationList.push( new Location(locationDetails));
+  });
 
 //Initiate the Map
-function initMap(){
-	//constructor to create a new map JS object.
-       map= new google.maps.Map(document.getElementById('map'), {
-        center: {lat:40.712775, lng: -74.005973},
-        zoom: 12,
-        //styles: styles        
-       });
-
+this.initMap=function (){
+  //constructor to create a new map JS object.
+        var mapCanvas = document.getElementById('map');
+        var mapOptions= {
+          center: new google.maps.LatLng(40.712775, -74.005973),
+          zoom: 15,
+          //styles: styles        
+       };
+  map = new google.maps.Map(mapCanvas, mapOptions);
     //create InfoWindow instance
-    var largeInfoWindow= new google.maps.InfoWindow();
+    this.largeInfoWindow= new google.maps.InfoWindow();
 
     //create marker color change
-    var markerDefaultIcon=makeMarkerIcon('0091ff');
-    var highlightedIcon=makeMarkerIcon('FFFF24');
+    this.markerDefaultIcon=makeMarkerIcon('0091ff');
+    this.highlightedIcon=makeMarkerIcon('FFFF24');
 
     //Create an Array to get all locations
     for(var i = 0;i < locations.length; i++) {
-    	// Get the position from the location array.
-        var position = locations[i].location;
-        var title = locations[i].title;
+      // Get the position from the location array.
+        this.position = locations[i].location;
+        this.title = locations[i].title;
         // Create a marker per location, and put into markers array.
-        var marker = new google.maps.Marker({
+        this.marker = new google.maps.Marker({
             position: position,
             title: title,
             animation: google.maps.Animation.DROP,
@@ -84,54 +99,55 @@ function initMap(){
             id: i
           });
         // Push the marker to our array of markers.
-        markers.push(marker);
+        this.markers.push(marker);
 
         //create LatLng bounds instance
-        var bounds = new google.maps.LatLngBounds();
+        this.bounds = new google.maps.LatLngBounds();
 
         // Extend the boundaries of the map for each marker and display the marker
-        for (var i = 0; i < markers.length; i++) {
-          markers[i].setMap(map);
-          bounds.extend(markers[i].position);
+        for (var j = 0; j <markers.length; j++) {
+          this.markers[j].setMap(map);
+          bounds.extend(markers[j].position);
+          map.fitBounds(bounds);
         }
 
         // Create an onclick event to open an infowindow at each marker.
-        marker.addListener('click', function() {
+        this.marker.addListener('click', function() {
             populateInfoWindow(this, largeInfoWindow);
           });
 
         //Create a listener for marker on mouseover highlighted icon.
-          marker.addListener('mouseover', function(){
-            this.setIcon(highlightedIcon);
+        this.marker.addListener('mouseover', function(){
+            self.setIcon(highlightedIcon);
           });
 
         //Create a listener for marker on mouseout default icon.
-          marker.addListener('mouseout', function(){
-            this.setIcon(markerDefaultIcon);
+        this.marker.addListener('mouseout', function(){
+            self.setIcon(markerDefaultIcon);
           });
     }
 }
 
 //Populate InfoWindow with title and Panorma image
-function populateInfoWindow(marker,infoWindow) {
+this.populateInfoWindow=function(marker,infoWindow) {
 
     // Check to make sure the infowindow is not already opened on this marker.
     if (infoWindow.marker != marker) {
-    	infoWindow.marker = marker;            
+      infoWindow.marker = marker;            
 
     // Make sure the marker property is cleared if the infowindow is closed.
-       	infoWindow.addListener('closeclick',function(){
-     		infoWindow.setMarker = null;
+        infoWindow.addListener('closeclick',function(){
+        infoWindow.setMarker = null;
         });
 
-       	//Create Streetview service instance
+        //Create Streetview service instance
         var streetViewService=new google.maps.StreetViewService();
         var radius=50;
 
         //Function for getting the streetview
         function getStreetView(data,status){
 
-        	//If status is ok,get a panorama street view location,heading
+          //If status is ok,get a panorama street view location,heading
             if (status===google.maps.StreetViewStatus.OK) {
               var nearStreetViewLocation=data.location.latLng;
               var heading=google.maps.geometry.spherical.computeHeading(nearStreetViewLocation, marker.position);
@@ -160,8 +176,8 @@ function populateInfoWindow(marker,infoWindow) {
 }
 
 //Function to set color for Marker icon
-function makeMarkerIcon(markerColor) {
-    var markerImage = new google.maps.MarkerImage(
+this.makeMarkerIcon=function(markerColor) {
+    this.markerImage = new google.maps.MarkerImage(
         'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
         '|40|_|%E2%80%A2',
         new google.maps.Size(34, 34),
@@ -171,4 +187,14 @@ function makeMarkerIcon(markerColor) {
         return markerImage;
 };
 
+this.initMap();
 
+}
+
+//Create Location function to hold location details
+var Location= function(data){
+  this.title=ko.observable(data.title);
+  this.img=ko.observable(data.img);
+}
+
+ko.applyBindings(new ViewModel());
